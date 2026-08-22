33af74ed6725cc62fb025926bb3c7f7e56a66d8a
juanzpp <juannsiilvah@gmail.com>
2026-08-22 04:06:44 -0300

    Migrate ESLint config for ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextVitals,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "coverage/**",
+    "desktop/src-tauri/target/**",
+    "next-env.d.ts",
+  ]),
+]);
