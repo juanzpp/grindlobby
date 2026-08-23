1c72759b33055ff4543c3a20e2271a81b04f2dbc
juanzpp <juannsiilvah@gmail.com>
2026-08-22 23:47:28 -0300

    Normalize desktop CSS during native bundle build

---
+++ desktop/ui/vite.config.mjs
@@ -0,0 +1,18 @@
+export default {
+  plugins: [
+    {
+      name: "grindlobby-desktop-css-contract",
+      enforce: "pre",
+      transform(code, id) {
+        if (!id.endsWith("styles.css")) return null;
+        const fixed = code
+          .replaceAll(".musicbar>div:nth-child(2)b{", ".musicbar>div:nth-child(2)>b{")
+          .replaceAll(".musicbar>div:nth-child(2)small{", ".musicbar>div:nth-child(2)>small{");
+        return { code: fixed, map: null };
+      }
+    }
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000
+  }
+};
