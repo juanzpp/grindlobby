86c95c9f6745ee252fa3d240bf3a8d5ca101f170
juanzpp <juannsiilvah@gmail.com>
2026-09-07 15:37:20 -0300

    feat(desktop): mount Pix checkout portal

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import BillingPortal from './BillingPortal'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <BillingPortal />
   </React.StrictMode>,
 )
