b02da43ae81fa0fa142e427e8330a8384c60c3bc
juanzpp <juannsiilvah@gmail.com>
2026-09-07 16:45:00 -0300

    feat(desktop): switch V3 runtime to clean login shell

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import V3App from './V3App'
+import V3Shell from './V3Shell'
 import './v3.css'
 import './v3-fidelity-overrides.css'
+import './v3-login-override.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <V3App />
+    <V3Shell />
   </React.StrictMode>,
 )
