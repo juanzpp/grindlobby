e3eee13f730cf0f0550368bfb6c484c894d4763d
juanzpp <juannsiilvah@gmail.com>
2026-09-07 16:17:09 -0300

    feat(desktop-v3): load reference fidelity layer

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import V3App from './V3App'
 import './v3.css'
+import './v3-fidelity-overrides.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
